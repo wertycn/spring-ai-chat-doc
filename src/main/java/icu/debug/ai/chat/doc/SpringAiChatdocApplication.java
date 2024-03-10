6e6513c0dccd87f40b24034a9efcd7f0a345fc31
debug.icu <hajnxg@126.com>
2024-03-10 16:49:35 +0800

    feat: project init

---
+++ src/main/java/icu/debug/ai/chat/doc/SpringAiChatdocApplication.java
@@ -0,0 +1,13 @@
+package icu.debug.ai.chat.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiChatdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiChatdocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/debug/ai/chat/doc/SpringAiChatdocApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.debug.ai.chat.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatdocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
